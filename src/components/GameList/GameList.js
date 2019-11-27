0d283404c4463bc691fdf2fc34f567afc69a5960
Nicolas Leport <nicolas.leport@nkcreation.com>
2019-11-27 12:12:21 +0100

    add apollo

--- src/components/GameList/GameList.js
+++ src/components/GameList/GameList.js
@@ -2,20 +2,48 @@ import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
+import gql from 'graphql-tag';
 import React from 'react';
+import { Query } from 'react-apollo';
 
 import styles from './GameList.module.scss';
 
+const GAME_QUERY = gql`
+  {
+    games(open: true) {
+      id
+      title
+    }
+  }
+`;
+
 export default function GameList() {
   return (
-    <div className={styles.gameList}>
-      <FormControl component="fieldset">
-        <h2 className={styles.h2}>Deux parties disponibles :</h2>
-        <RadioGroup aria-label="Deux parties disponibles" name="games">
-          <FormControlLabel value="1" control={<Radio />} label="NightClazz GraphQL" />
-          <FormControlLabel value="2" control={<Radio />} label="Une autre partie de test" />
-        </RadioGroup>
-      </FormControl>
-    </div>
+    <Query query={GAME_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Chargement des parties...</div>;
+        if (error) return <div>Erreur lors du chargement des parties, veuillez réessayer.</div>;
+
+        const gamesToRender = data.games;
+
+        const plural = gamesToRender > 1 ? 's' : '';
+
+        return (
+          <div className={styles.gameList}>
+            <FormControl component="fieldset">
+              {}
+              <h2 className={styles.h2}>
+                {gamesToRender.length} partie{plural} disponible{plural} :
+              </h2>
+              <RadioGroup aria-label="Deux parties disponibles" name="games">
+                {gamesToRender.map((game) => (
+                  <FormControlLabel key={game.id} value={game.id} control={<Radio />} label={game.title} />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </div>
+        );
+      }}
+    </Query>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import 'index.css';
 
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
 import App from 'components/App/App';
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 require('assets/scss/styles.scss');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
